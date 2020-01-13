c1a792bbaecd97136dbca81ccc61a5dae8f6ba43
Soumya Banerjee <neel.soumya@gmail.com>
2020-01-13 17:06:04 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # sc3_scater_single_cell_examples
 
+Examples of single-cell analysis using SC3 and scater
+
+Adapted from
+
+    * https://github.com/hemberg-lab/SC3
+    
+    * https://bioconductor.org/packages/release/bioc/vignettes/scater/inst/doc/overview.html
+    
---
+++ scater.R
@@ -0,0 +1,14 @@
+# https://bioconductor.org/packages/release/bioc/vignettes/scater/inst/doc/overview.html
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("scater")
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("SC3")
+
+# https://github.com/hemberg-lab/SC3
